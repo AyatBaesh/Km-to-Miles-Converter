6c85ac5f5c9f8d9a90bd33b07463c7aa13fb0607
AyatBaesh <111237017+AyatBaesh@users.noreply.github.com>
2022-12-06 02:22:09 +0100

    Added if for negative distance

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 import {convertKMtoMiles} from "./convert.js"
 const kmInput = document.querySelector("#km-input");
-const milesOutput = document.querySelector("#miles-output");
+let milesOutput = document.querySelector("#miles-output");
 const convertButton = document.querySelector("input[type='button']");
 
 kmInput.addEventListener("keyup",() => {
+    
+    let kmValue = document.querySelector("input[id='km-input'").value;
+    console.log(kmValue);
+    if(kmValue >= 0){
     milesOutput.value = convertKMtoMiles(kmInput.value);
-});
-convertButton.addEventListener("click",() => {
-    milesOutput.value = convertKMtoMiles(kmInput.value);
+    }else{
+        milesOutput.value = "Distance can't be negative"
+    }
 });
