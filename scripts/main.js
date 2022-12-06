c136b719ad292a84cb1b4604f10db934a4cdc1b7
Ayat <ayatbaeshev@gmail.com>
2022-12-06 20:06:20 +0100

    added convert button and made converter trigger on Enter

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,21 @@
 import {convertKMtoMiles} from "./convert.js"
 const kmInput = document.querySelector("#km-input");
 let milesOutput = document.querySelector("#miles-output");
-const convertButton = document.querySelector("input[type='button']");
+let convertButton = document.querySelector("#convert-button");
 
-kmInput.addEventListener("keyup",() => {
-    
-    let kmValue = document.querySelector("input[id='km-input'").value;
-    if(kmValue >= 0){
-    milesOutput.value = convertKMtoMiles(kmInput.value);
-    }else{
-        milesOutput.value = "Distance can't be negative"
+function convert(){
+
+        if(document.querySelector("#km-input").value >= 0){ //making sure that distance is not negative
+            return milesOutput.value = convertKMtoMiles(kmInput.value);
+        }else{
+            return milesOutput.value = "Distance can't be negative";
+        }
+}
+kmInput.addEventListener('keydown', (event) =>{
+    if(event.keyCode === 13){
+        convert();
     }
+    
 });
+convertButton.addEventListener('click', convert);
+
